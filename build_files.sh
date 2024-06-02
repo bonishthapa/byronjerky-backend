be317da3e15f65c48c024a0b6c3e626ac0a8e26d
Bonish Thapa <bonishthapa@gmail.com>
2024-06-02 23:29:25 +0545

    ssss

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,10 @@
 
-
-echo "Installing Django"
-python3.9 -m pip install Django
-
 echo "Building project...."
-python3.9 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
