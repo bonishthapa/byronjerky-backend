db39604f73764ca5fcddb61d20aa82abb63e1193
Bonish Thapa <bonishthapa@gmail.com>
2024-06-03 08:32:17 +0545

    status data added

--- app/stock/apis/dashboard.py
+++ app/stock/apis/dashboard.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta
-from django.db.models import Count
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -13,14 +12,28 @@ class DashboardApi(APIView):
         last_seven_days_order = (
             Order.objects.filter(is_deleted=False, created_date__gte=seven_days_ago.date()).prefetch_related(
                 "product", "customer").values(
-                    "created_date", "product__product_name", "customer__first_name", "customer__last_name").annotate(
-                        count=Count("id"))
+                    "created_date",
+                    "product__product_name",
+                    "customer__first_name",
+                    "customer__last_name",
+                    "status"
+                )
         )
         data = {}
+
         for entry in last_seven_days_order:
-            data[str(entry["created_date"])] = {
-                "count": entry["count"],
-                "product": entry["product__product_name"],
-                "customer": entry["customer__first_name"] + " " + entry["customer__last_name"],
-            }
+            if entry["status"] not in data:
+                data.update({
+                    entry["status"]: {}
+                })
+            if str(entry["created_date"]) in data[entry["status"]]:
+                data[entry["status"]][str(entry["created_date"])].append([{
+                    "product": entry["product__product_name"],
+                    "customer": entry["customer__first_name"] + " " + entry["customer__last_name"],
+                }])
+            else:
+                data[entry["status"]][str(entry["created_date"])] = [{
+                    "product": entry["product__product_name"],
+                    "customer": entry["customer__first_name"] + " " + entry["customer__last_name"],
+                }]
         return Response({'dashboard_data': data})
