9e1446dc63dfd40682911af75150416d7afa0db8
Bonish Thapa <bonishthapa@gmail.com>
2024-06-03 23:44:29 +0545

    updated dashbaord api

--- app/helpers/misc.py
+++ app/helpers/misc.py
@@ -1,76 +1,9 @@
-import base64
-import json
-
 from django.utils import timezone
-
-from Crypto.Cipher import AES
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import padding
-
-from .keys import get_public_key_object
+from datetime import timedelta
 
 
 def get_local_time():
     return timezone.now().astimezone(timezone.get_current_timezone())
-
-
-def encrypt_payload(public_key, data):
-    if not isinstance(data, bytes):
-        if isinstance(data, dict):
-            data = json.dumps(data)
-        data = data.encode()
-    if isinstance(public_key, str):
-        public_key = get_public_key_object(public_key)
-    ciphertext = public_key.encrypt(
-        data,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None,
-        ),
-    )
-    ciphertext = base64.b64encode(ciphertext).decode()
-    return ciphertext
-
-
-def decrypt_payload(private_key, ciphertext):
-    if not isinstance(ciphertext, bytes):
-        ciphertext = ciphertext.encode()
-    ciphertext = base64.b64decode(ciphertext)
-    plaintext = private_key.decrypt(
-        ciphertext,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None,
-        ),
-    )
-
-    return plaintext.decode()
-
-
-def aes_encrypt(encryption_key, payload):
-    if isinstance(payload, dict):
-        payload = json.dumps(payload)
-    if not isinstance(payload, str):
-        raise ValueError("Invalid payload")
-    encryption_key = encryption_key[:16]
-    length = 16 - (len(payload) % 16)
-    payload += chr(length) * length
-    obj = AES.new(encryption_key, AES.MODE_ECB)
-    ciphertext = obj.encrypt(payload)
-    ciphertext = base64.b64encode(ciphertext)
-    return ciphertext.decode()
-
-
-def aes_decrypt(encryption_key, ciphertext):
-    encryption_key = encryption_key[:16]
-    obj2 = AES.new(encryption_key, AES.MODE_ECB)
-    ciphertext = base64.b64decode(ciphertext)
-    plaintext = obj2.decrypt(ciphertext)
-    text = plaintext[: -plaintext[-1]]
-    text = text.decode()
-    return text
 
 
 def ip_is_allowed(ip_patterns, ip):
@@ -108,3 +41,11 @@ def get_chunks(data_list, n):
 
 def get_key_or_default(value_dict, key, default_value):
     return value_dict.get(key) or default_value
+
+
+def get_start_and_end_of_week(date=None, weekday_name=None):
+    date = date or get_local_time().date()
+    weekday = date.weekday()
+    start = date - timedelta(days=weekday)
+    end = start + timedelta(days=6)
+    return start, end
--- app/stock/apis/dashboard.py
+++ app/stock/apis/dashboard.py
@@ -1,22 +1,26 @@
-from datetime import datetime, timedelta
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from stock.models import Order
+from helpers.misc import get_start_and_end_of_week
 
 
 class DashboardApi(APIView):
     def get(self, request):
-        seven_days_ago = datetime.now() - timedelta(days=7)
+        start_date, end_date = get_start_and_end_of_week()
         last_seven_days_order = (
-            Order.objects.filter(is_deleted=False, created_date__gte=seven_days_ago.date()).prefetch_related(
+            Order.objects.filter(is_deleted=False, created_date__range=[start_date, end_date]).prefetch_related(
                 "product", "customer").values(
                     "created_date",
                     "product__product_name",
                     "customer__first_name",
                     "customer__last_name",
-                    "status"
+                    "status",
+                    "number_of_units",
+                    "wet_weight",
+                    "order_date",
+                    "production_date",
+                    "idx"
                 )
         )
         data = {}
@@ -27,13 +31,27 @@ class DashboardApi(APIView):
                     entry["status"]: {}
                 })
             if str(entry["created_date"]) in data[entry["status"]]:
-                data[entry["status"]][str(entry["created_date"])].append([{
+                data[entry["status"]][str(entry["created_date"])].append({
+                    "idx": entry["idx"],
                     "product": entry["product__product_name"],
                     "customer": entry["customer__first_name"] + " " + entry["customer__last_name"],
-                }])
+                    "created_date": entry["created_date"],
+                    "quantity": entry["number_of_units"],
+                    "wet_weight": entry["wet_weight"],
+                    "order_date": entry["order_date"],
+                    "production_date": entry["production_date"],
+
+                })
             else:
                 data[entry["status"]][str(entry["created_date"])] = [{
+                    "idx": entry["idx"],
                     "product": entry["product__product_name"],
                     "customer": entry["customer__first_name"] + " " + entry["customer__last_name"],
+                    "created_date": entry["created_date"],
+                    "quantity": entry["number_of_units"],
+                    "wet_weight": entry["wet_weight"],
+                    "order_date": entry["order_date"],
+                    "production_date": entry["production_date"],
                 }]
+
         return Response({'dashboard_data': data})
